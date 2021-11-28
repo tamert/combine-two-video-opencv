67003a5fa050b07591bc75c06d396d1134d90266
Tamer Ağaoğlu <tamer.a@90pixel.com>
2021-11-28 04:18:34 +0300

    added all bar

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 
 video1 = cv2.VideoCapture('samples/sample1.mp4')
 video2 = cv2.VideoCapture('samples/sample2.mp4')
-background = cv2.imread('samples/bg.jpeg')
+bar = cv2.imread('samples/bg.jpeg')
 logo = cv2.imread('samples/logo.jpeg')
 save_name = "output.mp4"
 fps = video1.get(cv2.CAP_PROP_FPS)
@@ -13,28 +13,6 @@ output_size = ((width * 2), height)
 
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 out = cv2.VideoWriter(save_name, fourcc, fps, output_size)
-
-
-def add_bg(bg, video, alpha=1.0, x=0, y=0, scale=1.0):
-    (h, w) = bg.shape[:2]
-    bg = np.dstack([bg, np.ones((h, w), dtype="uint8") * 255])
-    overlay = cv2.resize(video, None, fx=scale, fy=scale)
-    (wH, wW) = overlay.shape[:2]
-    output = bg.copy()
-
-    try:
-        if x < 0: x = w + x
-        if y < 0: y = h + y
-        if x + wW > w: wW = w - x
-        if y + wH > h: wH = h - y
-        print(x, y, wW, wH)
-        overlay = cv2.addWeighted(output[y:y + wH, x:x + wW], alpha, overlay[:wH, :wW], 1.0, 0)
-        output[y:y + wH, x:x + wW] = overlay
-    except Exception as e:
-        print("Error: Video position is overshooting bg!")
-        print(e)
-    output = output[:, :, :3]
-    return output
 
 
 def connect_frame(frame1, frame2, width, height):
@@ -49,20 +27,40 @@ while True:
 
     ret1, frame1 = video1.read()
     ret2, frame2 = video2.read()
-    print(ret1)
 
     if ret1 is not False and ret2 is not False:
 
         frame = connect_frame(frame1, frame2, width, height)
         frame = cv2.copyMakeBorder(frame, 20, 20, 20, 20, cv2.BORDER_CONSTANT, value=[255, 255, 255])
 
-
-        background = cv2.resize(background, (int(20), int(height+40)), interpolation=cv2.INTER_AREA)
-        img_height, img_width, _ = background.shape
+        # added left bar
+        y_bar = cv2.resize(bar, (int(20), int(height + 40)), interpolation=cv2.INTER_AREA)
+        x_bar = cv2.resize(bar, (int(40+width*2), int(20)), interpolation=cv2.INTER_AREA)
+        img_height, img_width, _ = y_bar.shape
         x = 0
         y = 0
-        frame[x:x+img_height, y:y+img_width] = background
+        frame[x:x + img_height, y:y + img_width] = y_bar
 
+        # added top bar
+        img_height, img_width, _ = x_bar.shape
+        x = 0
+        y = 0
+        frame[x:x + img_height, y:y + img_width] = x_bar
+
+        # added bottom bar
+        img_height, img_width, _ = x_bar.shape
+        x = 40+height-20
+        y = 0
+        frame[x:x + img_height, y:y + img_width] = x_bar
+
+        # added right bar
+        img_height, img_width, _ = y_bar.shape
+        x = 0
+        y = 40+width*2-20
+        frame[x:x + img_height, y:y + img_width] = y_bar
+
+
+        # add logo
         logo = cv2.resize(logo, (int(100), int(100)), interpolation=cv2.INTER_AREA)
         img_height, img_width, _ = logo.shape
         x = 10
